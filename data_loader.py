618287701c4c8546ec5d8c0dc7014705fb82e477
farazali7 <77faraz77@gmail.com>
2022-11-27 00:22:54 -0500

    Update data loader to only create subject dataframes as requested

--- data_loader.py
+++ data_loader.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import os
-import pickle
-import bz2
 
 
 def perform_col_swap(df, cols):
@@ -18,41 +16,39 @@ def perform_col_swap(df, cols):
     return df
 
 
-def create_full_subject_df(data_path):
+def create_subject_df(data_path, subject_num_str):
     '''
     Load data of all subjects into one dataframe with consistent column ordering.
-    :param data_path: String, path to directory containing subject data files
-    :return: DataFrame containing all subjects' data
+    :param data_path: String, path to directory containing all subject data files
+    :param subject_num_str: String, 3-digit subject number string to load data of (from '001' to '012')
+    :return: DataFrame containing requested subject's data
     '''
-    subjects_dict = {1: [], 2: [], 3: [], 4: [], 5: [], 6: [],
-                     7: [], 81: [], 82: [], 9: [], 10: [], 11: [], 12: []}
-    for path, subdirs, files in os.walk(data_path):
-        subdir_name = path.split('/')[-1]
-        if '008/OFF_1' in path:
-            subdir_name = 81
-        elif '008/OFF_2' in path:
-            subdir_name = 82
-        elif not subdir_name[0] == '0':
-            continue
-        subject_num = int(subdir_name)
-        for name in files:
-            if name[0] == '.':
-                continue
-            subjects_dict[subject_num].append(os.path.join(path, name))
-    subject_datasets = []
     # Create dictionary of required column swaps based on ordering in data's README to maintain consistency
     # Done for each subject as list of (col_name_1, col_name_2)
-    subjects_swaps = {1: [('EMG-LTA', 'EMG-RTA')], 2: [('EMG-LTA', 'EMG-RTA')], 3: [], 4: [], 5: [],
-                      6: [('EMG-LTA', 'EMG-RTA')], 7: [('EMG-LTA', 'EMG-RTA')], 81: [('EMG-LTA', 'EMG-RTA')],
-                      82: [('EMG-LTA', 'EMG-RGS'), ('EMG-RTA', 'EMG-RGS')], 9: [('ECG', 'EMG-RGS')],
-                      10: [], 11: [], 12: []}
-    for subject_num in subjects_dict.keys():
-        subject_paths = subjects_dict[subject_num]
-        for path in subject_paths:
+    subjects_swaps = {'001': [('EMG-LTA', 'EMG-RTA')], '002': [('EMG-LTA', 'EMG-RTA')], '003': [], '004': [], '005': [],
+                      '006': [('EMG-LTA', 'EMG-RTA')], '007': [('EMG-LTA', 'EMG-RTA')], '008': [('EMG-LTA', 'EMG-RTA')],
+                      '008_2': [('EMG-LTA', 'EMG-RGS'), ('EMG-RTA', 'EMG-RGS')], '009': [('ECG', 'EMG-RGS')],
+                      '010': [], '011': [], '012': []}
+    full_data_paths = []
+
+    # Handle subject 8 separately
+    if subject_num_str == '008':
+        full_data_paths.append(os.path.join(data_path, subject_num_str, 'OFF_1'))
+        full_data_paths.append(os.path.join(data_path, subject_num_str, 'OFF_2'))
+    else:
+        full_data_paths.append(os.path.join(data_path, subject_num_str))
+
+    subject_df_per_task = []
+    for i, full_data_path in enumerate(full_data_paths):
+        if i == 1:
+            subject_num_str = subject_num_str + '_2'
+        for file in os.listdir(full_data_path):
+            if file[0] == '.':
+                continue
+            path = os.path.join(full_data_path, file)
             task_num = int(path.split('/')[-1][-5])
             data = pd.read_csv(path, header=None)
             data = data.drop(data.iloc[:, 2:27], axis=1)
-            # TODO: Ordering of EMG data is switched for some patients... view data README
             df_col_names = ['Index', 'Time', 'EMG-LTA', 'EMG-RTA', 'IO', 'ECG', 'EMG-RGS',
                             'LS-ACCX', 'LS-ACCY', 'LS-ACCZ', 'LS-GYRX', 'LS-GYRY', 'LS-GYRZ', 'LS-NC/SC',
                             'RS-ACCX', 'RS-ACCY', 'RS-ACCZ', 'RS-GYRX', 'RS-GYRY', 'RS-GYRZ', 'RS-NC/SC',
@@ -60,41 +56,27 @@ def create_full_subject_df(data_path):
                             'A-ACCX', 'A-ACCY', 'A-ACCZ', 'A-GYRX', 'A-GYRY', 'A-GYRZ', 'A-NC/SC',
                             'FOG']
             data.columns = df_col_names
-            for swap in subjects_swaps[subject_num]:
+            for swap in subjects_swaps[subject_num_str]:
                 data = perform_col_swap(data, swap)
-            data['Subject'] = subject_num
+            data['Subject'] = subject_num_str
             data['Task'] = task_num
-            subject_datasets.append(data)
+            subject_df_per_task.append(data)
 
-    all_data = pd.concat(subject_datasets, axis=0, ignore_index=True)
-    cols = all_data.columns.tolist()
+    subject_df = pd.concat(subject_df_per_task, axis=0, ignore_index=True)
+    cols = subject_df.columns.tolist()
     cols = [cols[0]] + cols[-2:] + cols[1:-2]
-    all_data = all_data[cols]
+    subject_df = subject_df[cols]
 
-    return all_data
-
-
-def load_data(path):
-    '''
-    Load BZ2 compressed data file.
-    :param path: String, path to data
-    :returns: DataFrame
-    '''
-    data = bz2.BZ2File(path, 'rb')
-    data = pickle.load(data)
-
-    return data
+    return subject_df
 
 
 if __name__ == '__main__':
-    # data_path = 'data/Filtered Data'
-    # data = create_full_subject_df(data_path)
-    #
-    # # Compress and save
-    # sfile = bz2.BZ2File('all_subject_data', 'w')
-    # pickle.dump(data, sfile)
+    data_path = 'data/Filtered Data'
 
-    all_data_path = 'all_subject_data.pbz2'
-    data = load_data(all_data_path)
+    # Example: load subject 1 dataframe
+    subject1_df = create_subject_df(data_path, '001')
+
+    # Example: load subject 8 dataframe
+    subject8_df = create_subject_df(data_path, '008')
 
     print('done')
