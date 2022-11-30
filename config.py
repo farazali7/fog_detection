8f586fdec5eba7787225ba8c3637c18f2592959c
Marwan A Rahman <m2arahma@edu.uwaterloo.ca>
2022-11-30 18:45:36 -0500

    Adding training loop

---
+++ config.py
@@ -0,0 +1,50 @@
+from torch import nn, optim
+from torchvision import transforms
+
+cfg = {
+    'EPOCHS': 30,
+    'BATCH_SIZE': 16,
+    'SUBJECTS': [
+            "001",
+            "002",
+            "003",
+            "004",
+            "005",
+            "006",
+            "007",
+            "008",
+            "009",
+            "010",
+            "011",
+            "012"
+        ],
+    'MODALITIES': [
+            "EMG",
+            "ECG",
+            "ACC",
+            "GYR",
+            "NC/SC",
+            "IO",
+        ],
+    'LOSS': nn.CrossEntropyLoss,
+    'OPTIMIZER': 'adam',
+    'LR': 1e-3,
+    'WEIGHT_DECAY': 0,
+    'BETAS': [0.9, 0.999],
+    'EPSILON': 1e-8,
+    'MOMENTUM': 0.9,
+    'ROTATION_AUG': 10,
+    'SHEAR_AUG': 10,
+    'TRANSLATION_AUG': 0.2,
+    'CONTRAST_AUG': 0,
+    'FILTER_NUMS': [19, 38, 76, 152, 152, 152, 152],
+    'FILTER_SIZES': [5, 5, 5, 5, 5, 5, 5],
+    'STRIDES': [1, 1, 1, 1, 1, 1, 1],
+    'POOLINGS': [0, 0, 0, 0, 0, 0, 0],
+    'BATCH_NORM': [1, 1, 1, 1, 1, 1, 1],
+    'NUM_EPOCHS': 10,
+    'FC_SIZES': [40, 10],
+    'DROPOUT': [0, 0, 0, 0, 0, 0, 0],
+    'L2_REG': False,
+    'L1_REG': False
+}
--- main.py
+++ main.py
@@ -1,52 +1,121 @@
 # Training code goes here
 
 from data_loader import FOGDataset
+from models import CT_FOG
+import torch
+from torch.optim import Adam
 from torch.utils.data import DataLoader
 from torchvision import transforms
+from config import cfg
+from tqdm import tqdm
+from random import sample
+import numpy as np
 
+def train_val_test_split(subjects, train=10, val=1, test=1):
+    # split subjects into train/val/test
+    train_subjects = sample(subjects, train)
+    subjects = set(subjects) - set(train_subjects)
+    val_subjects = sample(subjects, val)
+    subjects = set(subjects) - set(val_subjects)
+    test_subjects = sample(subjects, test)
+    return train_subjects, val_subjects, test_subjects
 
 def main():
 
     # data downloaded from https://data.mendeley.com/datasets/r8gmbtv7w2/3 should be in this dir
     # ie there should be a 'data/Filtered Data' folder
     data_dir = "data"
+    train_subjects, val_subjects, test_subjects = train_val_test_split(cfg['SUBJECTS'])
 
-    fog_ds = FOGDataset(
+    train_ds = FOGDataset(
         data_dir=data_dir,
-        subjects=[
-            "001",
-            "002",
-            "003",
-            "004",
-            "005",
-            "006",
-            "007",
-            "008",
-            "009",
-            "010",
-            "011",
-        ],
-        modalities=[
-            "EMG",
-            "ECG",
-            "ACC",
-            "GYR",
-            "NC/SC",
-            "IO",
-        ],  # specify which we want from data_loader.MODALITIES
+        subjects=train_subjects,
+        modalities=cfg['MODALITIES'],  # specify which we want from data_loader.MODALITIES
         n_windows=1,
         transforms=transforms.ToTensor,
     )
 
-    loader = DataLoader(fog_ds, batch_size=16, shuffle=True)
+    val_ds = FOGDataset(
+        data_dir=data_dir,
+        subjects=val_subjects,
+        modalities=cfg['MODALITIES'],  # specify which we want from data_loader.MODALITIES
+        n_windows=1,
+        transforms=transforms.ToTensor,
+    )
 
-    for batch_i, batch in enumerate(loader):
-        x, y = batch
-        print(x.shape)  # (batch_size, n_windows, window_length, num_modalities)
-        print(y)  # 0 or 1
+    test_ds = FOGDataset(
+        data_dir=data_dir,
+        subjects=test_subjects,
+        modalities=cfg['MODALITIES'],  # specify which we want from data_loader.MODALITIES
+        n_windows=1,
+        transforms=transforms.ToTensor,
+    )
 
-        break
+    model = CT_FOG(in_channels=len(cfg['MODALITIES']), seq_len=train_ds.win_len)
 
+    #Added for GPU support 
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
+        model = model.to(torch.device("cuda:0"))
+
+    loss = cfg['LOSS']()
+    optimizer = Adam(
+            model.parameters(), 
+            cfg['LR'], 
+            betas=cfg['BETAS'],
+            eps=cfg['EPSILON'],
+            weight_decay=cfg['WEIGHT_DECAY']
+        )
+
+    train_loader = DataLoader(train_ds, batch_size=cfg['BATCH_SIZE'], shuffle=True)
+    val_loader = DataLoader(val_ds, batch_size=cfg['BATCH_SIZE'], shuffle=True)
+    losses = []
+    for epoch in tqdm(range(cfg['EPOCHS'])):
+        model.train()
+        num_correct = 0
+        for batch_i, batch in enumerate(train_loader):
+            batch_x, batch_y = batch
+            print(batch_x.shape)  # (batch_size, n_windows, window_length, num_modalities)
+            print(batch_y)  # 0 or 1
+
+            if torch.cuda.is_available():
+                batch_x = batch_x.to(torch.device("cuda:0"))
+                batch_y = batch_y.to(torch.device("cuda:0"))
+
+            output = model(batch_x)
+
+            batch_loss = loss(output, batch_y) # do we neeed y.long()?
+
+            preds = np.argmax(output.detach().numpy(), axis=-1)
+            num_correct += np.sum(preds == batch_y)
+
+            if torch.cuda.is_available():
+                batch_loss = batch_loss.to(torch.device("cpu"))
+            
+            losses.append(batch_loss.detach().numpy())
+
+            batch_loss.backward()
+            optimizer.zero_grad()
+            optimizer.step()
+        
+        print("train acc: ", num_correct / train_ds.num_samples)
+
+        num_correct = 0
+        model.eval()
+        with torch.no_grad():
+            for batch_i, batch in enumerate(val_loader):
+                batch_x, batch_y = batch
+                
+                if torch.cuda.is_available():
+                    batch_x = batch_x.to(torch.device("cuda:0"))
+                    batch_y = batch_y
+                    output = model(batch_x).to(torch.device("cpu"))
+                else:
+                    output = model(batch_x)
+
+                preds = np.argmax(output.detach().numpy(), axis=-1)
+                num_correct += np.sum(preds == batch_y)
+        print("val acc: ", num_correct / val_ds.num_samples)
 
 if __name__ == "__main__":
     main()
